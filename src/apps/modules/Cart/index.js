1f27486643a2f47f9ebe3baf446a8b7c6730798d
vothanhtinh <81636060+vothanhtinh@users.noreply.github.com>
2023-10-11 08:52:19 +0700

    chore : change method

--- src/apps/modules/Cart/index.js
+++ src/apps/modules/Cart/index.js
@@ -140,7 +140,7 @@ const Cart = () => {
     const areaTo = getArea(provinceCode, districtCode)
     fetch('https://services.giaohangtietkiem.vn/services/shipment/fee?', {
       mode: 'no-cors',
-      method: 'GET',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Token: '902db44af15b2551ae9212e426981dade3b86e19',
